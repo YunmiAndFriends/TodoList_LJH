9b8dcb8d44a90d8fc1e36369cf0618ccd803cf06
gpffh0001 <ekftn90@gmail.com>
2022-03-10 18:22:39 +0900

    feature: item 추가

--- todolist/src/page/TodoList.jsx
+++ todolist/src/page/TodoList.jsx
@@ -4,16 +4,33 @@ import styled from 'styled-components';
 
 function TodoList(){
     const [list, setList] = useState('')
+    const [item, setItem] = useState([])   // item의 초기값을 배열로 지정
+    let count = 0;
 
     const onChangeList = (e) => {
         setList(e.target.value)
     };
 
+    const onClickList = () => {
+        const todo = {
+            id: count,  // id(객체 구분)
+            content: list   // inputtodo 안에서 작성한 todo 내용
+        };
+        setItem(item.concat(todo));     // item 추가
+        setList('');
+    }
+
     return(
         <Container>
             <Title>TodoList</Title>
-            <MainTodo></MainTodo>
-            <InputTodo placeholder='Todo를 입력하세요.'></InputTodo>
+            <MainTodo>  {/*  */}
+                { item.map(({id, content})=><ContentDiv key={id}>{content}</ContentDiv>)}   {/* {id, content} == 하나의 요소로 ContentDiv의 키값(id)과 차일드 값(content), == const todo 값이라고 생각 */}
+            </MainTodo>
+            <AddDiv>    {/* 추가하는 부분 */}
+                <InputTodo onChange={onChangeList} placeholder='Todo를 입력하세요.'/>
+                <AddButton type='submit' onClick={onClickList}>전송</AddButton>
+            </AddDiv>
+            
         </Container>
 
     );
@@ -46,8 +63,21 @@ const Title = styled.h1`
 `;
 
 const InputTodo = styled.input`
-    width: 64vw;
+    width: 56vw;
     height: 4vh;
     font-size: 16px;
     text-align: center;
+`;
+
+const AddButton = styled.button`
+    width: 8vw;
+    height: 5vh;
+`;
+
+const AddDiv = styled.div`
+    display: flex;
+`;
+
+const ContentDiv = styled.div`
+    
 `;
