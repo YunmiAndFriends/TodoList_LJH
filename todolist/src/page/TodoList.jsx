a2c73c78b2751a24027dd8aae7573751ef22e5d5
gpffh0001 <ekftn90@gmail.com>
2022-03-10 19:44:08 +0900

    Add: 배운 내용 정리

--- todolist/src/page/TodoList.jsx
+++ todolist/src/page/TodoList.jsx
@@ -3,9 +3,9 @@ import { useState } from 'react'
 import styled from 'styled-components';
 
 function TodoList(){
-    const [list, setList] = useState('')
+    const [list, setList] = useState('')    // 상수(바뀌지 않는 값)
     const [item, setItem] = useState([])   // item의 초기값을 배열로 지정
-    let count = 0;
+    let count = 0;  // 변수(바뀌는 값)
 
     const onChangeList = (e) => {
         setList(e.target.value)
@@ -24,11 +24,13 @@ function TodoList(){
         <Container>
             <Title>TodoList</Title>
             <MainTodo>  {/*  */}
-                { item.map(({id, content})=><ContentDiv key={id}>{content}</ContentDiv>)}   {/* {id, content} == 하나의 요소로 ContentDiv의 키값(id)과 차일드 값(content), == const todo 값이라고 생각 */}
+                { item.map(({id, content})=><ContentDiv key={id}>{content}</ContentDiv>)}   
+                    {/* {id, content} == 하나의 요소로 ContentDiv의 키값(id)과 차일드 값(content), == const todo 값이라고 생각 */}
             </MainTodo>
             <AddDiv>    {/* 추가하는 부분 */}
-                <InputTodo onChange={onChangeList} placeholder='Todo를 입력하세요.'/>
+                <InputTodo onChange={onChangeList} placeholder='Todo를 입력하세요.' value={list}/>  {/* const onClickList의 setList(''); 값을 반영해서 값 초기화 */}
                 <AddButton type='submit' onClick={onClickList}>전송</AddButton>
+                <DeleteButton>전체 삭제</DeleteButton>
             </AddDiv>
             
         </Container>
@@ -70,8 +72,13 @@ const InputTodo = styled.input`
 `;
 
 const AddButton = styled.button`
-    width: 8vw;
-    height: 5vh;
+    width: 4vw;
+    height: 4.5vh;
+`;
+
+const DeleteButton = styled.button`
+    width: 4vw;
+    height: 4.5vh;
 `;
 
 const AddDiv = styled.div`
@@ -79,5 +86,6 @@ const AddDiv = styled.div`
 `;
 
 const ContentDiv = styled.div`
-    
+    color: white;
+    font-size: 22px;
 `;
